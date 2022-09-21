6f631e6e5dcd7aac77d3b42f404d0b6e18dffcba
akonst17 <akonst17@gmail.com>
2022-09-21 15:51:22 +0300

    интеграция sdk 2.0; новые урлы; build 6

--- GACE/Classes/Misc/GlobalDefinitions.swift
+++ GACE/Classes/Misc/GlobalDefinitions.swift
@@ -6,21 +6,24 @@
 //
 
 final class GlobalDefinitions {
-    static let domainUrl = "https://exams.korrekted.com"
+    static let domainUrl = "https://exams3.mastrapi.com"
     static let apiKey = "8APY7FEMI9EOYXdF"
-
-    static let sdkDomainUrl = "https://analytics.korrekted.com"
-    static let sdkApiKey = "6NcKuyuOjtSxEXj4"
 
     static let applicationTag = "EXAM_GACE"
 
     static let amplitudeApiKey = "b503251969f4b1d7901d2f7d1388d476"
     static let appsFlyerApiKey = "DCciCfYXjMQ8QnkdCg8qzk"
+    
+    static let sdkDomainUrl = "https://analytics.mastrapi.com"
+    static let sdkApiKey = "6NcKuyuOjtSxEXj4"
+    
+    static let otterScaleHost = "https://analytics2.mastrapi.com"
+    static let otterScaleApiKey = "4zPxxwvF6oOvBDf8"
 
     static let appStoreUrl = "https://apps.apple.com/app/gace-practice-test/id1591863459"
     static let appleAppID = "1591863459"
 
-    static let termsOfServiceUrl = "https://quantresear.ch/terms.html"
-    static let privacyPolicyUrl = "https://quantresear.ch/policy.html"
-    static let contactUsUrl = "https://quantresear.ch/contact.html"
+    static let termsOfServiceUrl = "https://mastrapi.com/terms"
+    static let privacyPolicyUrl = "https://mastrapi.com/policy"
+    static let contactUsUrl = "https://mastrapi.com/contact"
 }
